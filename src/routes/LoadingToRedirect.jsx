204400bd9965786361dd4a8dfa8b9c27ce536064
Siripat <67016676@rbac.ac.th>
2026-09-15 15:29:28 +0700

    init

--- src/routes/LoadingToRedirect.jsx
+++ src/routes/LoadingToRedirect.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react'
 import { Navigate } from 'react-router-dom'
 
 const LoadingToRedirect = () => {
-    const [count, setCount] = useState(10)
+    const [count, setCount] = useState(7)
     const [redireact, setRedireact] = useState(false)
 
     useEffect(() => {
